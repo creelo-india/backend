012923eaa09c4b5594d9784de85d230aa9297df7
sunilverma9695 <sunil.pythondev@gmail.com>
2024-09-22 23:35:19 +0530

    defdwF

--- creelo_india/settings.py
+++ creelo_india/settings.py
@@ -137,7 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/ubuntu/creelo/backend/staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
